f4a61ceab1daa3c4b0317449855d7acd7515b164
nileshbankar <nileshsbankar@gmail.com>
2021-06-02 19:23:57 +0530

    first commit

---
+++ src/components/Repos.js
@@ -0,0 +1,111 @@
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
+const Repos = () => {
+  const { repos } = useContext(GithubContext);
+
+  console.log("repos", repos);
+
+  let languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1, stars: stargazers_count };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
+      };
+    }
+
+    return total;
+  }, {});
+
+  const mostUsedLang = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .splice(0, 5);
+
+  // most stars per langauage
+
+  const mostPopularLang = Object.values(languages)
+    .sort((a, b) => {
+      return b.star - a.star;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
+    })
+    .splice(0, 5);
+
+  /// Repos and Stars
+
+  let { forks, stars } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+
+      return total;
+    },
+    { stars: {}, forks: {} }
+  );
+
+  stars = Object.values(stars).splice(-5).reverse();
+  forks = Object.values(forks).splice(-5).reverse();
+
+  const chartData1 = [
+    {
+      label: "HTML",
+      value: "122",
+    },
+    {
+      label: "Javascript",
+      value: "260",
+    },
+    {
+      label: "CSS",
+      value: "180",
+    },
+  ];
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        {/* <ExampleChart data={chartData}></ExampleChart> */}
+        <Pie3D data={mostUsedLang}></Pie3D>
+        <Column3D data={stars}></Column3D>
+        <Doughnut2D data={mostPopularLang}></Doughnut2D>
+        <Bar3D data={forks}></Bar3D>
+      </Wrapper>
+    </section>
+  );
+};
+
+const Wrapper = styled.div`
+  display: grid;
+  justify-items: center;
+  gap: 2rem;
+  @media (min-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (min-width: 1200px) {
+    grid-template-columns: 2fr 3fr;
+  }
+
+  div {
+    width: 100% !important;
+  }
+  .fusioncharts-container {
+    width: 100% !important;
+  }
+  svg {
+    width: 100% !important;
+    border-radius: var(--radius) !important;
+  }
+`;
+
+export default Repos;
---
+++ src/context/context.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
+
+const rootUrl = "https://api.github.com";
+
+const GithubContext = React.createContext();
+
+const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState({ show: false, msg: "" });
+  const [rateLimit, setRateLimit] = useState({ remaining: 60, limit: 60 });
+
+  const fetchUser = async (githubUser) => {
+    toggleError();
+    setIsLoading(true);
+    const result = await axios
+      .get(`${rootUrl}/users/${githubUser}`)
+      .catch((err) => console.log(err));
+
+    if (result) {
+      const userData = result.data;
+
+      setGithubUser(userData);
+
+      // Fetch repo
+
+      const url = `${rootUrl}/users/${userData.login}/repos?per_page=100`;
+      const repoResponse = await axios
+        .get(url)
+        .catch((err) => console.log(err));
+      setRepos(repoResponse.data);
+
+      // Fetch followers
+
+      const followerResponse = await axios
+        .get(userData.followers_url)
+        .catch((err) => console.log(err));
+      setFollowers(followerResponse.data);
+    } // const userData = await result.data;
+    else {
+      toggleError(true, `User does not found for search test ${githubUser}`);
+    }
+    setIsLoading(false);
+  };
+
+  const fetchRequestRate = async () => {
+    const url = `${rootUrl}/rate_limit`;
+    const result = await axios.get(url).catch((err) => console.log(err));
+    if (result.status === 200) {
+      const rate_limit = result.data;
+
+      if (rate_limit.rate.remaining > 0) {
+        setRateLimit({
+          remaining: rate_limit.rate.remaining,
+          limit: rate_limit.rate.limit,
+        });
+      } else {
+        setRateLimit({
+          remaining: 0,
+          limit: rate_limit.rate.limit,
+        });
+        toggleError(true, "sorry, you have exeeded your hourly limit!");
+      }
+    } // const userData = await result.data;
+    else {
+      console.error("No rate limit found");
+    }
+  };
+
+  //fetchUser("nileshsbankar");
+  // fetchRequestRate();
+
+  const toggleError = (show = false, msg = "") => {
+    setError({ show, msg });
+  };
+
+  useEffect(() => {
+    fetchRequestRate();
+  }, []);
+
+  return (
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        rateLimit,
+        fetchUser,
+        error,
+        isLoading,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export { GithubProvider, GithubContext };
